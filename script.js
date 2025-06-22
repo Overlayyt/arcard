03e9668a49bdaf226f7ee1e4640aa1fe8c2157bb
Overlayyt <overlayai.yt@gmail.com>
2025-06-22 17:29:31 +0000

    F17

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ faceMesh.onResults((results) => {
     }
 
     if (currentMode === 'necklace' && necklaceImg && chinSmooth) {
-      canvasCtx.drawImage(necklaceImg, chinSmooth.x - 100, chinSmooth.y, 300, 150);
+      canvasCtx.drawImage(necklaceImg, chinSmooth.x - 120, chinSmooth.y, 300, 150);
     }
   }
 });
@@ -158,7 +158,7 @@ function takeSnapshot() {
 
   if (currentMode === 'necklace' && necklaceImg) {
     const chinSmooth = smooth(chinPositions);
-    if (chinSmooth) ctx.drawImage(necklaceImg, chinSmooth.x - 100, chinSmooth.y, 300, 150);
+    if (chinSmooth) ctx.drawImage(necklaceImg, chinSmooth.x - 120, chinSmooth.y, 300, 150);
   }
 
   lastSnapshotDataURL = snapshotCanvas.toDataURL('image/png');
